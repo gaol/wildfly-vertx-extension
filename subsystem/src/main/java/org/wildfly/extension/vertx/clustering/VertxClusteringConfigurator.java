0bfa9ce7cd012af91dada440cd99d6acf9fcd414
Lin Gao <aoingl@gmail.com>
2021-12-30 14:49:01 +0800

    Removed unused files

--- subsystem/src/main/java/org/wildfly/extension/vertx/clustering/VertxClusteringConfigurator.java
+++
@@ -1,6 +0,0 @@
-package org.wildfly.extension.vertx.clustering;
-
-public class VertxClusteringConfigurator {
-    // see: org.jboss.as.ejb3.cache.distributable.DistributableCacheFactoryBuilderServiceConfigurator
-    // see: org.jboss.as.ejb3.subsystem.PassivationStoreAdd.install()
-}

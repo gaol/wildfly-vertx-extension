abfb7ab1649acc1bb3e744ee52da8c3c8c13348c
Lin Gao <aoingl@gmail.com>
2022-01-18 11:47:19 +0800

    Added tests on Vertx file system API

---
+++ testsuite/integration/mini/src/test/java/org/wildfly/extension/vertx/test/mini/deployment/VertxFileSystemTestCase.java
@@ -0,0 +1,68 @@
+/*
+ *  Copyright (c) 2022 The original author or authors
+ *
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of Apache License v2.0 which
+ *  accompanies this distribution.
+ *
+ *       The Apache License v2.0 is available at
+ *       http://www.opensource.org/licenses/apache2.0.php
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
+package org.wildfly.extension.vertx.test.mini.deployment;
+
+import io.vertx.core.json.JsonObject;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.as.test.integration.common.HttpRequest;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.extension.vertx.test.shared.StreamUtils;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Test accessing resources inside of the deployment using vertx filesystem API.
+ *
+ * @author <a href="mailto:aoingl@gmail.com">Lin Gao</a>
+ */
+@RunWith(Arquillian.class)
+@RunAsClient
+public class VertxFileSystemTestCase {
+
+    @ArquillianResource
+    private URL url;
+
+    @Deployment(testable = false)
+    public static Archive<?> deployment() {
+        WebArchive web = ShrinkWrap.create(WebArchive.class, "test-vertx-filesystem.war")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "vertx-deployment.json")
+                .addAsResource("assets")
+                .addAsResource("geo.json")
+                .addClasses(FileAccessServelt.class, StreamUtils.class);
+        return web;
+    }
+
+    @Test
+    public void testEchoAsyncServlet() throws Exception {
+        String res = HttpRequest.get( url.toExternalForm() + "file-access", 10, TimeUnit.SECONDS);
+        JsonObject json = new JsonObject(res);
+        Assert.assertEquals("Beijing", json.getJsonObject("geo.json").getString("location"));
+        Assert.assertEquals("2022", json.getJsonObject("config").getString("covid.year.vanish"));
+    }
+
+}
---
+++ testsuite/shared/src/main/java/org/wildfly/extension/vertx/test/shared/StreamUtils.java
@@ -0,0 +1,121 @@
+/*
+ *  Copyright (c) 2022 The original author or authors
+ *
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of Apache License v2.0 which
+ *  accompanies this distribution.
+ *
+ *       The Apache License v2.0 is available at
+ *       http://www.opensource.org/licenses/apache2.0.php
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
+package org.wildfly.extension.vertx.test.shared;
+
+import io.vertx.core.json.JsonObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+
+/**
+ * Utils class on stream string conversion.
+ *
+ * @author <a href="mailto:aoingl@gmail.com">Lin Gao</a>
+ */
+public class StreamUtils {
+
+    /**
+     * Reads InputStream and converts to a String using UTF-8 encoding.
+     *  This won't close the stream.
+     *
+     * @param input the input stream
+     * @return the String representation of the content reads from the stream.
+     * @throws IOException Exception that may occur
+     */
+    public static String streamToString(InputStream input) throws IOException {
+        return streamToString(input, false);
+    }
+
+    /**
+     * Reads InputStream and converts to a String using UTF-8 encoding.
+     *
+     * @param input the input stream
+     * @param closeStream if the stream should be closed at the end
+     * @return the String representation of the content reads from the stream.
+     * @throws IOException Exception that may occur
+     */
+    public static String streamToString(InputStream input, boolean closeStream) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8));
+        try {
+            String line;
+            while((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+        } finally {
+            if (closeStream) {
+                reader.close();
+                input.close();
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Converts the content read from the stream to a JsonObject.
+     *
+     * @param input the input stream
+     * @param closeStream if the stream should be closed at the end
+     * @return a JsonObject instance with the String representation of the content reads from the stream.
+     * @throws IOException Exception that may occur
+     */
+    public static JsonObject streamToJsonObject(InputStream input, boolean closeStream) throws IOException {
+        return new JsonObject(streamToString(input, closeStream));
+    }
+
+    /**
+     * Converts the content read from the stream to a Properties.
+     *
+     * @param input the input stream
+     * @param closeStream if the stream should be closed at the end
+     * @return a Properties instance with the String representation of the content reads from the stream.
+     * @throws IOException Exception that may occur
+     */
+    public static Properties streamToProperties(InputStream input, boolean closeStream) throws IOException {
+        Properties props = new Properties();
+        try {
+            props.load(input);
+        } finally {
+            if (closeStream) {
+                input.close();
+            }
+        }
+        return props;
+    }
+
+    /**
+     * Converts String representation to a Properties instance.
+     *
+     * @param data string content that can be converted to a Properties
+     * @return a Properties instance
+     */
+    public static Properties stringToProperties(String data)  {
+        Properties props = new Properties();
+        try (StringReader sr = new StringReader(data)) {
+            props.load(sr);
+        } catch (IOException ioe) {
+            ;// ignore
+        }
+        return props;
+    }
+}

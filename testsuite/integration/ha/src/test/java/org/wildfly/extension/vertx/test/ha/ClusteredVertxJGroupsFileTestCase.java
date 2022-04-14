2ad5758177c7ac0eccfc7b68ad676fdabc4753ed
Lin Gao <aoingl@gmail.com>
2022-04-14 20:12:21 +0800

    Added a test case of clustered Vertx with customized jgroups  file

--- testsuite/integration/ha/src/test/java/org/wildfly/extension/vertx/test/ha/ClusteredVertxJGroupsFileTestCase.java
+++ testsuite/integration/ha/src/test/java/org/wildfly/extension/vertx/test/ha/ClusteredVertxJGroupsFileTestCase.java
@@ -16,11 +16,31 @@
  */
 package org.wildfly.extension.vertx.test.ha;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.concurrent.TimeUnit;
+import io.netty.util.internal.logging.InternalLoggerFactory;
+import io.netty.util.internal.logging.JdkLoggerFactory;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.impl.VertxBuilder;
+import io.vertx.ext.cluster.infinispan.InfinispanClusterManager;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.as.arquillian.api.ServerSetup;
+import org.jboss.as.arquillian.container.ManagementClient;
+import org.jboss.as.test.integration.common.HttpRequest;
+import org.jboss.as.test.integration.management.util.ServerReload;
+import org.jboss.as.test.shared.SnapshotRestoreSetupTask;
+import org.jboss.dmr.ModelNode;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
 
 import javax.inject.Inject;
 import javax.servlet.AsyncContext;
@@ -29,48 +49,69 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.concurrent.TimeUnit;
 
-import io.netty.util.internal.logging.InternalLoggerFactory;
-import io.netty.util.internal.logging.JdkLoggerFactory;
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.test.api.ArquillianResource;
-import org.jboss.as.arquillian.api.ServerSetup;
-import org.jboss.as.arquillian.container.ManagementClient;
-import org.jboss.as.test.integration.common.HttpRequest;
-import org.jboss.as.test.shared.SnapshotRestoreSetupTask;
-import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import io.vertx.core.Promise;
-import io.vertx.core.Vertx;
-import io.vertx.core.VertxOptions;
-import io.vertx.core.impl.VertxBuilder;
-import io.vertx.ext.cluster.infinispan.InfinispanClusterManager;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ADD;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.VALUE;
+import static org.wildfly.extension.vertx.test.shared.ManagementClientUtils.addVertxOperation;
+import static org.wildfly.extension.vertx.test.shared.ManagementClientUtils.executeOperation;
+import static org.wildfly.extension.vertx.test.shared.ManagementClientUtils.removeVertxOperation;
 
 /**
  * This is a test covering the case that messages flowing in the event bus between 2 Vertx instances.
  * <p>
- * It starts with a http request to a servlet which will send a string message to address 'inbox', the
- * consumer in client side will be triggered which replies the message so that the server side gets the reply message
- * and responds to the http request with the status check.
+ * It is similar with {@link ClusteredVertxTestCase}, but this test case tries to set up a new clustered Vertx instance
+ * using a customized <i>test-jgroups-stack.xml</i> file.
  * </p>
  * @author <a href="mailto:aoingl@gmail.com">Lin Gao</a>
  */
 @RunWith(Arquillian.class)
-@ServerSetup(ClusteredVertxTestCase.ClusteredTestSetupTask.class)
-public class ClusteredVertxTestCase {
+@ServerSetup(ClusteredVertxJGroupsFileTestCase.ClusteredTestSetupTask.class)
+public class ClusteredVertxJGroupsFileTestCase {
+
+  @ClassRule
+  public static TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+  private static final String CLUSTERED_VERTX_NAME = "clusterVertx";
 
   public static class ClusteredTestSetupTask extends SnapshotRestoreSetupTask {
     private Vertx clusteredVertx;
+    private String tmpFileLoc;
+
+    private static final String JGROUPS_FILE_PING_LOCATION = "jgroups.file.location";
+    private static final String JGROUPS_FILE_LOCATION = "test-jgroups-stack.xml";
 
     @Override
     public void doSetup(ManagementClient managementClient, String containerId) throws Exception {
+      tmpFileLoc = temporaryFolder.newFolder().getAbsolutePath();
+      // remove the default vertx instance
+      executeOperation(managementClient, removeVertxOperation("default"));
+      ServerReload.executeReloadAndWaitForCompletion(managementClient.getControllerClient());
+
+      System.setProperty("vertx.jgroups.config", JGROUPS_FILE_LOCATION);
+      System.setProperty(JGROUPS_FILE_PING_LOCATION, tmpFileLoc);
+
+      // set system property of jgroups.file.location to a temp dir for FILE_PING protocol
+      ModelNode operation = new ModelNode();
+      operation.get(OP_ADDR).set(new ModelNode().add("system-property", JGROUPS_FILE_PING_LOCATION));
+      operation.get(OP).set(ADD);
+      operation.get(VALUE).set(tmpFileLoc);
+      executeOperation(managementClient, operation);
+
+      // setup vertx with jgroups-stack-file=test-jgroups-stack.xml
+      ModelNode addVertxOperation = addVertxOperation(CLUSTERED_VERTX_NAME);
+      addVertxOperation.get("clustered").set(true);
+      addVertxOperation.get("jgroups-stack-file").set(getClass().getClassLoader().getResource(JGROUPS_FILE_LOCATION).getFile());
+      executeOperation(managementClient, addVertxOperation);
+
+      ServerReload.executeReloadAndWaitForCompletion(managementClient.getControllerClient());
+
       InternalLoggerFactory.setDefaultFactory(JdkLoggerFactory.INSTANCE);
       Promise<Vertx> promise = Promise.promise();
       VertxOptions vertxOptions = new VertxOptions();
@@ -92,6 +133,8 @@ public class ClusteredVertxTestCase {
 
     @Override
     public void nonManagementCleanUp() throws Exception {
+      System.clearProperty("vertx.jgroups.config");
+      System.clearProperty(JGROUPS_FILE_PING_LOCATION);
       clusteredVertx.close().toCompletionStage().toCompletableFuture().get();
     }
   }
@@ -101,9 +144,9 @@ public class ClusteredVertxTestCase {
 
   @Deployment
   public static Archive<?> deployment() throws Exception {
-    return ShrinkWrap.create(WebArchive.class, "test-send-and-check.war")
+    return ShrinkWrap.create(WebArchive.class, "test-send-and-check-jgroups-stack-file.war")
       .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
-      .addClasses(ClusteredVertxTestCase.class, SendMessageAndCheckServlet.class)
+      .addClasses(ClusteredVertxJGroupsFileTestCase.class, SendMessageAndCheckServlet.class)
       .addPackage(HttpRequest.class.getPackage());
   }
 
@@ -112,13 +155,13 @@ public class ClusteredVertxTestCase {
 
     // in ha configuration, it is a clustered vertx instance
     @Inject
-    private Vertx vertx;
+    private Vertx clusterVertx;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
       String message = req.getParameter("message") == null || req.getParameter("message").length() == 0 ? "Ni Hao" : req.getParameter("message");
       final AsyncContext asyncContext = req.startAsync();
-      vertx.eventBus()
+      clusterVertx.eventBus()
         .request("inbox", message)
         .toCompletionStage()
         .whenComplete((m, e) -> {

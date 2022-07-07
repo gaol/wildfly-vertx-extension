de117decf2d29a7b81bed43ca2f2ac61477a1b58
Lin Gao <aoingl@gmail.com>
2022-07-07 14:26:08 +0800

    Added vertx-mongo-client

---
+++ testsuite/integration/mini/src/test/resources/init-mongo.js
@@ -0,0 +1,5 @@
+db.exam.insertMany([
+             { name: "Chinese Language", score: 99 },
+             { name: "Math", score: 98 },
+             { name: "English Language", score: 100 }
+          ])
